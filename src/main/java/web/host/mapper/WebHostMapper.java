19a3e2d63fd8e91c6bc6dbfa83ca42413599b977
rajatyadavcse <56014534+rajatyadavcse@users.noreply.github.com>
2021-05-15 20:17:05 +0530

    database added

--- src/main/java/web/host/mapper/WebHostMapper.java
+++ src/main/java/web/host/mapper/WebHostMapper.java
@@ -2,11 +2,7 @@ package web.host.mapper;
 
 import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
-import web.host.dbadapter.dao.LinksDAO;
-import web.host.dbadapter.dao.SideNavDAO;
 import web.host.dbadapter.dao.TaskDAO;
-import web.host.model.LinksRequest;
-import web.host.model.SideNavRequest;
 import web.host.model.Task;
 
 import java.util.List;
@@ -14,10 +10,6 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 @Component
 public interface WebHostMapper {
-    SideNavRequest toSideNavRequest(SideNavDAO sideNavDAO);
-    LinksRequest toLinksRequest(LinksDAO linksDAO);
-    SideNavDAO toSideNavDAO(SideNavRequest sideNavRequest);
-    LinksDAO toLinksDAO(LinksRequest linksRequest);
     TaskDAO toTaskDAO(Task task);
     List<Task> toTaskList(List<TaskDAO> taskDAOList);
     Task toTask(TaskDAO taskDAO);
